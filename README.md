219cc7fbd69f2910c5170da4388fc8cbea1c9f94
Bill Siever <bsiever@gmail.com>
2016-02-21 07:51:32 -0600

    Added suggestion for exercise to update scrolling algorithm.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SimbleeForMobile-BarGraph
 SimbleeForMobile library for a simple scrolling BarGraph.
+
+This library uses an inefficient scroll update method: the bars are at
+fixed locations on the screen and every update causes the width and height
+of each to update.  A better approach would be to adjust just the x 
+coordinate of all the bars that need to "move left" on a scroll and 
+update the width, height, and x coordinate of the leftmost bar to become
+the new rightmost bar.
--- src/BarGraph.cpp
+++ src/BarGraph.cpp
@@ -5,6 +5,18 @@
 // The location and size are configured when it appears. 
 // The number of bars, maxValue, and colors are specified when declared.
 
+
+/*
+    EXERCISE
+    
+    This library uses an inefficient scroll update method: the bars are at
+    fixed locations on the screen and every update causes the width and height
+    of each to update.  A better approach would be to adjust just the x 
+    coordinate of all the bars that need to "move left" on a scroll and 
+    update the width, height, and x coordinate of the leftmost bar to become
+    the new rightmost bar. 
+*/
+
 BarGraph::BarGraph(int bars, unsigned maxValue, color_t backgroundColor, color_t barColor): 
    x(0), y(0), width(0), height(0), bars(bars), 
    currentBars(0), maxValue(maxValue), barColor(barColor), backgroundColor(backgroundColor) { 
